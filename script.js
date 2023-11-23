4c49703b812f11f00ee4555772745273babf7878
Victoria McCray <69064355+victoriamccray@users.noreply.github.com>
2023-11-22 20:22:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const svg = d3.select("#game-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
 // Changes Character Size with Slider
 var characterSize = d3.select("#slider").property("value");
 var initialCharacterY = height / 2 - rectHeight / 2; // Adjusted for vertical centering
@@ -26,44 +31,33 @@ d3.select("#jump").on("click", function () {
 });
 
 var characterY = d3.select(".character").attr("y");
-        
-    //animates blue block from rightmost part of screen to leftmost screen
+
+// animates blue block from rightmost part of the screen to the leftmost screen
 function runBlock() {
-
-
     var block = svg.append("rect")
-                .attr("x", width)
-                .attr("y", height/2 + 100)
-                .attr("width", squareSize)
-                .attr("height", squareSize)
-                .attr("fill", "blue")
-                .attr("class","block")
-                .transition()
-                .duration(2000)
-                .ease(d3.easeLinear)
-                    .attr("x",90)
-                    .remove()
-                    .on("end", function() {
-                        timeElapsed = timeElapsed + 2000;
-                        
-                        if(timeElapsed >= 2000 && d3.select(".character").attr("y") > 350) {
-                            character.attr("opacity", 0);
-                            d3.select("#status").html("You lost!");
-                        } else {
-                            runBlock();
-                        }
-                    });
-                    
+        .attr("x", width)
+        .attr("y", height / 2 + 100)
+        .attr("width", squareSize)
+        .attr("height", squareSize)
+        .attr("fill", "blue")
+        .attr("class", "block")
+        .transition()
+        .duration(2000)
+        .ease(d3.easeLinear)
+        .attr("x", 90)
+        .remove()
+        .on("end", function () {
+            timeElapsed = timeElapsed + 2000;
 
+            if (timeElapsed >= 2000 && d3.select(".character").attr("y") > 350) {
+                character.attr("opacity", 0);
+                d3.select("#status").html("You lost!");
+            } else {
+                runBlock();
+            }
+        });
 
-            console.log(timeElapsed);
-
+    console.log(timeElapsed);
 }
 
 runBlock();
-
-d3.select("#restart").on("click", function() {
-    character.attr("opacity", 1);
-    runBlock();
-    d3.select("#status").html("Jump over the blue block!");
-});
